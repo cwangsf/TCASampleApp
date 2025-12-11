8f7f605284c8113e54b3e6e1d29a36f03617b885
Cynthia Wang <cynthiazw@gmail.com>
2025-12-11 10:11:03 +0000

    Initial check in.

---
+++ TCASampleApp/TCASampleApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TCASampleApp
+//
+//  Created by Cynthia Wang on 12/11/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ TCASampleApp/TCASampleAppTests/TCASampleAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TCASampleAppTests.swift
+//  TCASampleAppTests
+//
+//  Created by Cynthia Wang on 12/11/25.
+//
+
+import Testing
+@testable import TCASampleApp
+
+struct TCASampleAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
